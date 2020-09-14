d1043fcf1f0e11d0c7db200561b15eebc2d49120
wadkan <zsoltgaramszegi@gmail.com>
2020-09-14 17:19:59 +0200

    Gladiator factory DONE

--- src/main/java/com/codecool/gladiator/model/gladiators/GladiatorFactory.java
+++ src/main/java/com/codecool/gladiator/model/gladiators/GladiatorFactory.java
@@ -1,9 +1,12 @@
 package com.codecool.gladiator.model.gladiators;
 
+import com.codecool.gladiator.util.RandomUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.Random;
 
 public class GladiatorFactory {
 
@@ -13,7 +16,7 @@ public class GladiatorFactory {
         try {
             File file = new File(getClass().getClassLoader().getResource(fileOfNames).getFile());
             names = Files.readAllLines(file.toPath());
-        } catch (IOException|NullPointerException e) {
+        } catch (IOException | NullPointerException e) {
             System.out.println("Names file not found or corrupted!");
             System.exit(1);
         }
@@ -33,10 +36,33 @@ public class GladiatorFactory {
      * Instantiates a new gladiator with random name and type.
      * Creating an Archer, an Assassin, or a Brutal has the same chance,
      * while the chance of creating a Swordsman is the double of the chance of creating an Archer.
+     *
      * @return new Gladiator
      */
     public Gladiator generateRandomGladiator() {
-        // Todo
-        return new Brutal(getRandomName(), 50, 50, 50, 1);
+        Random RANDOM = new Random();
+//        RandomUtils RANDOM = new RandomUtils();
+        int gladiatorType = RANDOM.nextInt(5);
+        int baseHp = RANDOM.nextInt(101 - 25) + 25;
+        int baseSp = RANDOM.nextInt(101 - 25) + 25;
+        int baseDex = RANDOM.nextInt(101 - 25) + 25;
+        int baseLevel = RANDOM.nextInt(6 - 1) + 1;
+
+
+        System.out.println("random gladiatorType is: " + gladiatorType);
+
+        switch (gladiatorType) {
+            case 0:
+                return new Archer(getRandomName(), baseHp, baseSp, baseDex, baseLevel);
+            case 1:
+                return new Assassin(getRandomName(), baseHp, baseSp, baseDex, baseLevel);
+            case 2:
+                return new Brutal(getRandomName(), baseHp, baseSp, baseDex, baseLevel);
+            case 3:
+            case 4:
+                return new Swordsman(getRandomName(), baseHp, baseSp, baseDex, baseLevel);
+            default:
+                throw new IllegalStateException("Unexpected value: " + gladiatorType);
+        }
     }
 }
